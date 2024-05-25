e39abb75f375b2facefa02c2b53926d8777950fb
meeb <meeb@meeb.org>
2024-05-25 19:38:53 +1000

    bump libs

--- demoblog/settings.py
+++ demoblog/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django_distill',
     'django_cachekiller',
     'blog',
